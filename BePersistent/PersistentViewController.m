f1235029003b41604a9eb1c051a10b43926fd6ab
Ross McIlwaine <rdm0423@gmail.com>
2015-02-10 10:46:51 -0700

    Dictionary set up

--- BePersistent/PersistentViewController.m
+++ BePersistent/PersistentViewController.m
@@ -21,8 +21,23 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    NSDictionary *player = [[NSUserDefaults standardUserDefaults] objectForKey:@"player"];
+    [self updateWithPlayerDictionary:player];
+    
 }
 
+- (void)updateWithPlayerDictionary:(NSDictionary *) player {
+    
+    self.playerNameTextField.text = player[@"nameKey"];
+    self.scoreLabel.text = player[@"scoreKey"];
+    
+    //have to create a number as below we have it as a number value. casting it.
+    NSNumber *stepperValue = player[@"scoreKey"];
+    self.stepper.value = [stepperValue doubleValue];
+                                
+    
+}
 
 - (IBAction)saveButton:(id)sender {
     NSMutableDictionary *playerDictionary = [NSMutableDictionary new];
@@ -30,6 +45,8 @@
     playerDictionary[@"nameKey"] = self.playerNameTextField.text;
     [playerDictionary setObject:@(self.stepper.value) forKey:@"scoreKey"];
     
+    [[NSUserDefaults standardUserDefaults] setObject:playerDictionary forKey:@"player"];
+    [[NSUserDefaults standardUserDefaults] synchronize];  //allows us to save down
     
 
 }
